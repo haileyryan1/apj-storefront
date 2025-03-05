0cc82d3e268459bd0de4df1a27925db8caa20872
Hailey Ryan <hailey.ryan1@gmail.com>
2025-03-05 13:50:09 -0700

    Consuming Web Services Attempt 1

---
+++ tutorial9-2/src/main/java/edu/byui/apj/storefront/tutorial92/AnimalClientApplication.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial92;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalClientApplication.class, args);
+    }
+
+}
