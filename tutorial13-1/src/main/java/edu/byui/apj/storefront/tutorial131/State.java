103a1cabebe7dd74bbd40e6f5763f53ef86bbe2f
Hailey Ryan <hailey.ryan1@gmail.com>
2025-04-02 14:15:16 -0600

    W11 updates

---
+++ tutorial13-1/src/main/java/edu/byui/apj/storefront/tutorial131/State.java
@@ -0,0 +1,22 @@
+package edu.byui.apj.storefront.tutorial131;
+
+import java.awt.image.BufferedImage;
+
+class State {
+    private final BufferedImage baseImage;
+    private final int hue;
+
+    public State(BufferedImage baseImage, int hue) {
+        this.baseImage = baseImage;
+        this.hue = hue;
+    }
+
+    public BufferedImage getBaseImage() {
+        return baseImage;
+    }
+
+    public int getHue() {
+        return hue;
+    }
+
+}
---
+++ tutorial13-1/src/main/java/edu/byui/apj/storefront/tutorial131/Tutorial131Application.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial131;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial131Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial131Application.class, args);
+	}
+
+}
